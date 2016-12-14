de2d9ea7e97963e1d7dfe096c1bf38a5011f0c44
Mikel Ereño <MEreno1995@hotmail.com>
2016-12-14 23:37:24 +0100

    Interfaz empezada

---
+++ ListaTareas/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  ListaTareas
+//
+//  Created by Mikel Ereño on 13/12/16.
+//  Copyright © 2016 Mikel Ereño. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
